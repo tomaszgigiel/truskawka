fc47993c0c77966ca5b70fc7da38563408b0f46b
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2018-08-01 20:54:48 +0200

    core functionality

--- doc/intro.md
+++ doc/intro.md
@@ -1,3 +1,4 @@
 # Introduction to truskawka
 
 https://alvinalexander.com/blog/post/java/jget-something-like-wget
+https://stackoverflow.com/questions/15628682/in-clojure-how-do-you-download-an-image-from-the-web-and-save-it-to-your-file-s
--- src/main/clojure/pl/tomaszgigiel/truskawka/core.clj
+++ src/main/clojure/pl/tomaszgigiel/truskawka/core.clj
@@ -1,7 +1,16 @@
 (ns pl.tomaszgigiel.truskawka.core
+  (:require [clojure.java.io :as io])
+  (:import java.io.File)
+  (:import java.net.URL)
   (:gen-class))
 
+(defn copy-uri-to-file [uri file]
+  (with-open [in (io/input-stream uri)
+              out (io/output-stream file)]
+    (io/copy in out)))
+
 (defn -main
-  "I don't do a whole lot."
+  "truskawka: clojure wget"
   [& args]
-  (println "Hello, World!"))
+  (copy-uri-to-file (new URL (first args)) (new File (second args)))
+  (println "ok"))
